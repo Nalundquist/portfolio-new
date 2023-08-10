80351895d01249101e6d2f5e03b6002d062b51e0
Noah Lundquist <nalundquist@gmail.com>
2023-08-10 13:20:03 -0700

    adjust/add seeds, start leaf clip path

--- app/components/Strawberry.tsx
+++ app/components/Strawberry.tsx
@@ -2,29 +2,32 @@ import React from "react";
 
 const Strawberry: React.FC = () => {
   const seedPositions = [
-    { top: 24, left: -42 },
-    { top: 12, left: -10 },
-    { top: 15, left: -50 },
-		{ top: 20, left: -42 },
-    { top: 12, left: -10 },
+    { top: 4, left: 89 },
+    { top: 8, left: 23 },
+    { top: 15, left: -30 },
+		{ top: 30, left: -12 },
+    { top: 12, left: 40 },
     { top: 65, left: -50 },
 		{ top: 24, left: -42 },
     { top: 52, left: -60 },
-    { top: 15, left: -50 },
-		{ top: 24, left: -42 },
-    { top: 42, left: -10 },
-    { top: 45, left: -70 }
+    { top: 75, left: -140 },
+		{ top: 87, left: -142 },
+    { top: 42, left: -180 },
+    { top: 65, left: -120 },
+		{ top: 104, left: -182 },
+    { top: 42, left: -140 },
+    { top: 95, left: -190 }
   ];
 
   return (
-    <div className='absolute flex h-[18rem] w-[18rem] justify-center transform rotate-[30deg] scale-[1.45] ' style={{ top: 150, right: 280 }}>
+    <div className='absolute flex h-[18rem] w-[18rem] justify-center transform rotate-[30deg] scale-[1.45] ' style={{ top: 200, right: 280 }}>
       <div className='flex flex-column relative h-[10rem] w-full bg-red-600 flex justify-center flex-row clip-path-strawberry'>
         {seedPositions.map((position, index) => (
           <div
             key={index}
-            className="bg-amber-500 w-[20px] h-[30px] relative clip-path-seed"
+            className="bg-lime-200 w-[20px] h-[30px] relative clip-path-seed"
             style={{
-              transform: `translate(${position.left}px, ${position.top}px) scale(.7)`,
+              transform: `translate(${position.left}px, ${position.top}px) scale(.6)`,
             }}
           ></div>
         ))}
