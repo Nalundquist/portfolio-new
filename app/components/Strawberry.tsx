fe23f7642befeaa10c8d9fc5c5dfcc552afa7d6c
Noah Lundquist <nalundquist@gmail.com>
2023-08-09 22:56:41 -0700

    add Strawberry Component; clip-path for said

---
+++ app/components/Strawberry.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Strawberry: React.FC = () => {
+	return(
+		<div className='relative flex justify-center'>
+			<div className=''></div>
+		</div>
+	)
+}
+
+export default Strawberry;
