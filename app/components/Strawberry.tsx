7f3c6f0d3975c545a038ecb9d5e9b1b22a17bdd5
Noah Lundquist <nalundquist@gmail.com>
2023-08-09 23:14:26 -0700

    add strwbry to main page, begin building component

--- app/components/Strawberry.tsx
+++ app/components/Strawberry.tsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Strawberry: React.FC = () => {
 	return(
-		<div className='relative flex justify-center'>
-			<div className=''></div>
+		<div className='absolute flex h-[32rem] w-[18rem] justify-center transform rotate-[45deg] ' style={{top: 150, right: 280}}>
+			<div className='relative h-[10rem] w-full bg-red-400 rounded-full flex justify-center flex-row'></div>
+				<div className="relative h-full w-full bg-red-400 transform clip-path-strawtip"></div>
 		</div>
 	)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PieCircle from './components/PieCircle';
+import Strawberry from './components/Strawberry';
 
 const Home: React.FC = () => {
   return (
     <div className="flex items-center bg-pink-200 justify-center h-screen">
       <PieCircle />
+      <Strawberry />
     </div>
   );
 };
